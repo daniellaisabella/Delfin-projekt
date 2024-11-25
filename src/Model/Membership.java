9b789fd0c58f345a01f810fd83c7aed59f04814d
daniellaisabella <dato0001@stud.kea.dk>
2024-11-25 11:50:28 +0100

    Membership added

--- src/Model/Membership.java
+++ src/Model/Membership.java
@@ -1,20 +1,49 @@
 package Model;
 
-public class Membership extends Member {
+    public class Membership extends Member {
 
-    public Membership(String name, int age, boolean active, boolean junior, boolean fitnessEnthusiast) {
-        super(name, age, active, junior, fitnessEnthusiast);
+        // Enum to categorize membership types
+        private MembershipType membershipType;
+
+        // Constructor
+        public Membership(String name, int age, boolean active, boolean fitnessEnthusiast, MembershipType membershipType) {
+            super(name, age, active, fitnessEnthusiast); // Call to the Member class constructor
+            this.membershipType = membershipType;
+        }
+
+        // Getter and Setter for membership type
+        public MembershipType getMembershipType() {
+            return membershipType;
+        }
+
+        public void setMembershipType(MembershipType membershipType) {
+            this.membershipType = membershipType;
+        }
+
+        // Calculate contingent based on membership type
+        @Override
+        public double calculateContingent() {
+            switch (membershipType) {
+                case PASSIVE:
+                    return 500.0;
+                case ACTIVE_JUNIOR:
+                    return 1000.0;
+                case ACTIVE_SENIOR:
+                    return 1600.0;
+                case ACTIVE_PENSIONIST:
+                    return 1200.0;
+                default:
+                    throw new IllegalArgumentException("Invalid membership type");
+            }
+        }
+
+        // Override toString for detailed information
+        @Override
+        public String toString() {
+            return String.format(
+                    "Name: %s, Age: %d, Membership Type: %s, Active: %b, Fitness Enthusiast: %b, Contingent: %.2f DKK",
+                    getName(), getAge(), membershipType, isActive(), isFitnessEnthusiast(), calculateContingent()
+            );
+        }
     }
 
-    @Override
-    public double calculateContingent() {
-        return 0;
-    }
-
-    //PASSIVE - 500 DKK
-    //ACTIVE JUNIOR - 1000 DKK
-    //ACTIVE SENIOR - 1600 DKK
-    //ACTIVE PENSIONIST - 1200 DKK
-    //Constructor -
-
-}
