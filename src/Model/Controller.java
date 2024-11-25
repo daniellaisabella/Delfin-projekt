0f4afa7ad0a4820331358b730274bcc632526088
haiq0001 <haiq0001@stud.kea.dk>
2024-11-25 11:55:51 +0100

    Controller class edited

--- src/Model/Controller.java
+++ src/Model/Controller.java
@@ -1,4 +1,16 @@
 package Model;
+import java.util.ArrayList;
 
 public class Controller {
+    private MemberList memberList;
+
+    public Controller() {
+        memberList = new MemberList(new ArrayList<>());
+    }
+
+    public MemberList getMemberList() {
+        return memberList;
+    }
+
+
 }
