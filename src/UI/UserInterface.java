89abc47406fdd7af96eeb0daddc1dd5a46b85e36
haiq0001 <haiq0001@stud.kea.dk>
2024-11-25 10:46:26 +0100

    startProgram method created

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -16,6 +16,23 @@ public class UserInterface {
         System.out.println("Type 'add' to add a member to the swimming club");
         System.out.println("Type 'members' to show the member list");
     }
+
+    public void startProgram() {
+        boolean running = true;
+        displayMenu();
+
+        try {
+            while (running) {
+                String choice = scanner.nextLine().trim().toLowerCase();
+
+                switch (choice) {
+
+                    case "add", "1" -> addMember();
+                    case "Members", "2" -> showMembers();
+
+                }
+
+
 }
 
 
