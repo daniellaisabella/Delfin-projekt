a3ef359a3cad1187e7eb97134147b0230b358f48
haiq0001 <haiq0001@stud.kea.dk>
2024-11-25 10:43:05 +0100

    displayMenu method created

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -1,4 +1,26 @@
 package UI;
+import model.Controller;
+import java.util.Scanner;
 
 public class UserInterface {
+    private Controller controller;
+    private Scanner scanner;
+
+    public UserInterface() {
+        this.controller = new Controller();
+        this.scanner = new Scanner(System.in);
+    }
+
+    void displayMenu() {
+        System.out.println("\n---- Administation ----");
+        System.out.println("Type 'add' to add a member to the swimming club");
+        System.out.println("Type 'members' to show the member list");
+    }
 }
+
+
+
+
+
+
+
