4e751adf1cd8e733edad749db072343fe0803d47
irpo0001@stud.kea.dk <irpo0001@stud.kea.dk>
2024-12-02 11:03:19 +0100

    birthdate to age

--- src/Model/Swimmer.java
+++ src/Model/Swimmer.java
@@ -1,14 +1,15 @@
 package Model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Swimmer extends Member {
     private List<CompetitiveResults> competitiveResults;
 
-    public Swimmer(String name, String surName, LocalDate birthDate, boolean isActive,
+    public Swimmer(String name, String surName, LocalDate age, boolean isActive,
                    String address, int phoneNumber, String email, boolean isCompetitive) {
-        super(name, surName, birthDate, address, phoneNumber, email, isActive, isCompetitive);
+        super(name, surName, age, address, phoneNumber, email, isActive, isCompetitive);
         this.competitiveResults = new ArrayList<>();
     }
 
