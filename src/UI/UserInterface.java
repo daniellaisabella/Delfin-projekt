ce10e06da73ea9cc296eb9f170ae1fc2c0dc9a7b
daniellaisabella <dato0001@stud.kea.dk>
2024-12-03 12:40:26 +0100

    FEJL. Member list count when Show member [2]

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -13,9 +13,16 @@ public class UserInterface {
     private Filehandler filehandler = new Filehandler();
 
     public UserInterface() {
+
+    }
+
+    public boolean load2() {
         ArrayList<Swimmer> loadedMembers = filehandler.loadMembers();
         if (loadedMembers != null) {
             controller.getMemberList().getMembers().addAll(loadedMembers);
+            return true; // Loading successful
+        } else {
+            return false; // Loading failed
         }
     }
 
@@ -150,10 +157,7 @@ public class UserInterface {
     private void showMembers() {
         ArrayList<Member> members = controller.getMemberList().getMembers();
 //        System.out.println("Debug: Number of members in the list: " + members.size());
-        ArrayList<Swimmer> loadedMembers = filehandler.loadMembers();
-        if (loadedMembers != null) {
-            controller.getMemberList().getMembers().addAll(loadedMembers);
-        }
+
         System.out.println("Number of members in the list: " + members.size());
 
         if (members.isEmpty()) {
@@ -164,6 +168,7 @@ public class UserInterface {
                 System.out.println(member);
             }
         }
+
     }
 
     private void displayTreasurerMenu() {
